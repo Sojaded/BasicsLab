4df80432a7d1a46e71d2d1b8ccc98aaa7040dab4
Sojaded <kelvin.kaweru@strathmore.edu>
2023-10-28 15:37:27 +0300

    Basic compose essentials

---
+++ app/src/main/java/com/example/basicslab/ui/theme/Color.kt
@@ -0,0 +1,8 @@
+package com.example.basicslab.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val Navy = Color(0xFF03042)
+val Blue = Color(0xFF4385F4)
+val LightBlue = Color(0xFFD7EFFE)
+val Chartreuse = Color ( 0xFFEFF7CF)
